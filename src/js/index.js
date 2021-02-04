5925c543f170a43931305494bbddf6819ff8dca5
Louis <l.clisson@hotmail.fr>
2021-02-04 18:19:19 +0100

    feat: add function to generate random names

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,17 @@
 import '../sass/styles.scss'
-import App from './particule/scripts/App';
+import Shuffle from './services/shuffle.js'
 import LocomotiveScroll from 'locomotive-scroll';
 import Cursor from './menu/cursor';
 
 
 
 const APP = window.APP || {}
-
+// let names = new Shuffle();
+// console.log(names.NAMES);
+// console.log(names.shuffle())
+// console.log(names.shuffle())
+// console.log(names.shuffle())
+// console.log(names.shuffle())
 
 const initApp = () => {
     window.APP = APP
