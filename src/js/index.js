878c93c35953b8ea93754ffa6d4ca61a213c507f
Simon Ebel <51062314+Parpinio@users.noreply.github.com>
2021-02-04 17:57:43 +0100

    Push master

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+import '../sass/styles.scss'
+import App from './particule/scripts/App';
+import LocomotiveScroll from 'locomotive-scroll';
+import Cursor from './menu/cursor';
+
+
+
+const APP = window.APP || {}
+
+
+const initApp = () => {
+    window.APP = APP
+}
+
+if (document.readyState === 'complete' || (document.readyState !== 'loading' && !document.documentElement.doScroll)) {
+    initApp()
+} else {
+    document.addEventListener('DOMContentLoaded', initApp)
+}
